e6b8cf08847213a7b314c4fee98d7ea994dbad83
pablogipi <pablo@rohtau.com>
2022-11-14 16:57:58 +0100

    Added compiled modules for treesitter parsers so we can install them later without need of compiling again. Added gitattributes to about costly diff of binary files.

--- nvim/devenv.cmd
+++ nvim/devenv.cmd
@@ -2,17 +2,19 @@
 ::
 @echo off
 
+echo %~dp0
+
 :: Point to our dev env
-set XDG_CONFIG_HOME=c:\dev
-set XDG_DATA_HOME=c:\dev
+:: set XDG_CONFIG_HOME=c:\dev
+:: set XDG_DATA_HOME=c:\dev
 
 :: Add macro to use vi as nvim and gvim as nvim-qt
-doskey vi=nvim $*
-doskey gvim=nvim-qt $*
-doskey gv=nvim-qt $*
+::doskey vi=nvim $*
+::doskey gvim=nvim-qt $*
+::doskey gv=nvim-qt $*
 
 :: Set nvim as default editor
-set EDITOR=nvim
-set VISUAL=nvim
-set GIT_EDITOR=nvim
+::set EDITOR=nvim
+::set VISUAL=nvim
+::set GIT_EDITOR=nvim
 
---
+++ nvim/treesitter_parsers/README.txt
@@ -0,0 +1,5 @@
+These folders contains the compilable modules for Treesitter parsers and their config files.
+Just copy them into ...\nvim-data\site\pack\packer\start\nvim-treesitter\parser
+there are separated into windows and linux platforms.
+This wil allow to install these modules without the need of compile anything.
+Just run :Mason to install them.
